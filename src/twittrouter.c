0f92f35fc54d1910a089150b8fda61a1dba119eb
scola <friend_21@yahoo.cn>
2014-10-05 14:06:47 +0800

    use select to replace thread

--- src/twittrouter.c
+++ src/twittrouter.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/select.h>
 
 #include "twittrouter.h"
 #include "utils.h"
@@ -13,22 +12,6 @@ jconf_t *conf = NULL;
 char *root = NULL;
 char *servPort = NULL;
 linklist arpList = NULL;
-
-void *ThreadMain(void *arg); // Main program of a thread
-void *ScanArpList(void); // a thread to get arp list
-
-// Structure of arguments to pass to client thread
-struct ThreadArgs {
-    int clntSock; // Socket descriptor for client
-};
-
-static void create_thread(void *thread_func,void *threadArgs) {
-    pthread_t threadID;
-    int returnValue = pthread_create(&threadID, NULL, thread_func, threadArgs);
-    if (returnValue != 0)
-        FATAL("pthread_create() failed");
-    printf("with thread %ld\n", (long int) threadID);
-}
 
 int main(int argc, char *argv[]) {
     int auth_flags = 0;
@@ -118,54 +101,50 @@ int main(int argc, char *argv[]) {
     
     //we need to create a thread to scan arp list and block some ip.
     arpList = CreatEmptyLink();
-    create_thread(ScanArpList,NULL);
     printf("server listening at port %s...\n",servPort);
 
     int servSock = SetupTCPServerSocket(servPort);
     if (servSock < 0)
         FATAL("unable to establish");
+        
+    fd_set readset, errorset;
+    int max_fd = servSock + 1;
+    int loop_count = 0;
     for (;;) { // Run forever
-        int clntSock = AcceptTCPConnection(servSock);
-
-        // Create separate memory for client argument
-        struct ThreadArgs *threadArgs = (struct ThreadArgs *) malloc(
-                sizeof(struct ThreadArgs));
-        if (threadArgs == NULL)
-            FATAL("malloc() failed");
-        threadArgs->clntSock = clntSock;
-
-        // Create client thread
-        create_thread(ThreadMain,(void *)threadArgs);
+        FD_ZERO(&readset);
+        FD_ZERO(&errorset);
+        FD_SET(servSock, &readset);
+        FD_SET(servSock, &errorset);
+        struct timeval timeout = {
+          .tv_sec = 1,
+          .tv_usec = 0,
+        };
+        
+        if (-1 == select(max_fd, &readset, NULL, &errorset, &timeout)) {
+            ERROR("select");
+        }
+        
+        if (FD_ISSET(servSock, &errorset)) {
+            // TODO getsockopt(..., SO_ERROR, ...);
+            ERROR("servSock error\n");
+        }
+        
+        if(FD_ISSET(servSock, &readset)) {
+            int clntSock = AcceptTCPConnection(servSock);
+            HandleTCPClient(clntSock);
+        }
+        if(loop_count % 10 == 0){
+            int fd;
+            char buf[BUFSIZE] = {0};
+            if ((fd=open("/proc/net/arp", O_RDONLY)) != -1)    //FILE FOUND
+            {        
+                read(fd, buf, BUFSIZE);
+                scan_arp_and_block(buf); 
+                close(fd);
+            }
+            loop_count = 0;
+        }
+        loop_count++;
     }
     // NOT REACHED
-}
-
-void *ThreadMain(void *threadArgs) {
-    // Guarantees that thread resources are deallocated upon return
-    pthread_detach(pthread_self());
-
-    // Extract socket file descriptor from argument
-    int clntSock = ((struct ThreadArgs *) threadArgs)->clntSock;
-    free(threadArgs); // Deallocate memory for argument
-
-    HandleTCPClient(clntSock);
-
-    return (NULL);
-}
-
-void *ScanArpList(void) {
-    // Guarantees that thread resources are deallocated upon return
-    pthread_detach(pthread_self());
-    sleep(5);
-    for(;;) {
-        int fd;
-        char buf[BUFSIZE] = {0};
-        if ((fd=open("/proc/net/arp", O_RDONLY)) != -1)    //FILE FOUND
-        {        
-            read(fd, buf, BUFSIZE);
-            scan_arp_and_block(buf); 
-            close(fd);
-        }        
-        sleep(10);
-    }
 }
